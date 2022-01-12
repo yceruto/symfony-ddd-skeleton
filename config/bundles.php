bd9b02236ccbc88ddc69937391e7d859885d398a
Yonel Ceruto <yonelceruto@gmail.com>
2022-01-12 00:14:06 -0500

    Added yceruto/ddd-maker-bundle package as dev requirement

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Yceruto\DddMakerBundle\DddMakerBundle::class => ['dev' => true],
 ];
